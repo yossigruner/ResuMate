8640d2a2d8cc5b7ddb74f2d6da63e72856eac869
Oron Ben Zvi <oronbz@gmail.com>
2016-03-04 13:27:42 +0200

    added basic landing page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('landing', { title: 'Resumate', layout: null });
+});
+
+/* GET resume */
+router.get('/resume', function(req, res, next) {
+  res.render('resume', { title: 'Resumate' });
 });
 
 module.exports = router;
