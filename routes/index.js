ed26f91e18defd17084b5a87c0b15fd88e801edd
Oron Ben Zvi <oronbz@gmail.com>
2016-03-06 11:18:15 +0200

    added public link

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,30 @@
+var User = require('../models/user');
 
 module.exports = function(app, passport) {
   app.get('/', function(req, res) {
     if (req.isAuthenticated()) {
-        console.log(req.user);
-        res.render('resume', {
-          user: req.user
-        });
+      console.log(req.user.id);
+      res.render('resume', {
+        user: req.user
+      });
     } else {
       res.render('landing', { layout: null });
     }
+  });
+
+  app.get('/resume/:id', function(req, res, next) {
+    var id = req.params.id;
+    User.findById(id, function(err, user) {
+      if (err) {
+        console.error(err);
+        return res.sendStatus(500);
+      }
+      if (!user) {
+        return res.sendStatus(404);
+      }
+      res.render('resume', { user: user, public: true });
+    });
+
   });
 
   var auth = require('../routes/auth')(app, passport);
